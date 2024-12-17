8c23fbaeeba8e65aa133861b3a9a01b5e57de43d
LoxXo <kubawa19@gmail.com>
2024-12-17 23:35:35 +0100

    changes to function name, disabled AddName1

--- api/function_app.py
+++ api/function_app.py
@@ -8,57 +8,57 @@ app = func.FunctionApp()
 load_dotenv("config.env")
 
 
-@app.function_name(name="AddName1")
-@app.route(route="add_name", auth_level=func.AuthLevel.ANONYMOUS, methods=["POST"])
-@app.queue_output(
-    arg_name="msg", queue_name="outqueue", connection="AzureWebJobsStorage"
+# @app.function_name(name="AddName1")
+# @app.route(route="add_name", auth_level=func.AuthLevel.ANONYMOUS, methods=["POST"])
+# @app.queue_output(
+#     arg_name="msg", queue_name="outqueue", connection="AzureWebJobsStorage"
+# )
+# @app.cosmos_db_output(
+#     arg_name="outputDocument",
+#     database_name=os.environ["COSMOS_DATABASE"],
+#     container_name=os.environ["COSMOS_CONTAINER"],
+#     connection="CosmosDbConnectionSetting",
+# )
+# def add_name(
+#     req: func.HttpRequest,
+#     msg: func.Out[func.QueueMessage],
+#     outputDocument: func.Out[func.Document],
+# ) -> func.HttpResponse:
+#     """azure basic function to test output for POST"""
+#     logging.info("Python HTTP trigger function processed a request.")
+#     logging.info("Python Cosmos DB trigger function processed a request.")
+#     name = req.params.get("name")
+#     if not name:
+#         try:
+#             req_body = req.get_json()
+#         except ValueError:
+#             pass
+#         else:
+#             name = req_body.get("name")
+
+#     if name:
+#         outputDocument.set(func.Document.from_dict({"id": name}))
+#         msg.set(name)  # type: ignore
+#         return func.HttpResponse(f"Hello {name}!")
+#     else:
+#         return func.HttpResponse(
+#             "Please pass a name on the query string or in the request body",
+#             status_code=400,
+#         )
+
+
+@app.function_name(name="new_visitor")
+@app.route(route="new_visitor", auth_level=func.AuthLevel.ANONYMOUS)
+@app.cosmos_db_input(
+    arg_name="inputDocument",
+    database_name=os.environ["COSMOS_DATABASE"],
+    container_name=os.environ["COSMOS_CONTAINER"],
+    connection="CosmosDbConnectionSetting",
 )
 @app.cosmos_db_output(
     arg_name="outputDocument",
     database_name=os.environ["COSMOS_DATABASE"],
     container_name=os.environ["COSMOS_CONTAINER"],
-    connection="CosmosDbConnectionSetting",
-)
-def add_name(
-    req: func.HttpRequest,
-    msg: func.Out[func.QueueMessage],
-    outputDocument: func.Out[func.Document],
-) -> func.HttpResponse:
-    """azure basic function to test output for POST"""
-    logging.info("Python HTTP trigger function processed a request.")
-    logging.info("Python Cosmos DB trigger function processed a request.")
-    name = req.params.get("name")
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get("name")
-
-    if name:
-        outputDocument.set(func.Document.from_dict({"id": name}))
-        msg.set(name)  # type: ignore
-        return func.HttpResponse(f"Hello {name}!")
-    else:
-        return func.HttpResponse(
-            "Please pass a name on the query string or in the request body",
-            status_code=400,
-        )
-
-
-@app.function_name(name="NewVisitor")
-@app.route(route="new_visitor", auth_level=func.AuthLevel.ANONYMOUS)
-@app.cosmos_db_input(
-    arg_name="inputDocument",
-    database_name=os.environ["COSMOS_DATABASE"],
-    container_name=os.environ["COSMOS_CONTAINER_COUNTER"],
-    connection="CosmosDbConnectionSetting",
-)
-@app.cosmos_db_output(
-    arg_name="outputDocument",
-    database_name=os.environ["COSMOS_DATABASE"],
-    container_name=os.environ["COSMOS_CONTAINER_COUNTER"],
     connection="CosmosDbConnectionSetting",
     create_if_not_exists=True,
     partition_key="id",
@@ -88,12 +88,12 @@ def new_visitor(
     return f"All time visitor number: {new_number}"
 
 
-@app.function_name(name="GetVisitors")
+@app.function_name(name="get_visitors")
 @app.route(route="get_visitors", auth_level=func.AuthLevel.ANONYMOUS)
 @app.cosmos_db_input(
     arg_name="inputDocument",
     database_name=os.environ["COSMOS_DATABASE"],
-    container_name=os.environ["COSMOS_CONTAINER_COUNTER"],
+    container_name=os.environ["COSMOS_CONTAINER"],
     connection="CosmosDbConnectionSetting",
 )
 def get_visitors(
