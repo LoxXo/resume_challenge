7955ffc8319da970b163632508a00e25c1385cc0
LoxXo <kubawa19@gmail.com>
2024-12-28 01:08:55 +0100

    fixed in CSP header, formating in visitcounter.js

--- webpage/visitcounter.js
+++ webpage/visitcounter.js
@@ -2,4 +2,4 @@ var data
 window.onload = fetch('https://http-trigger-cosmos-resume.azurewebsites.net/api/new_visitor?')
     .then(response => console.log(response.status) || response)
     .then(response => (data = response.text()))
-    document.getElementById("visitors").innerHTML = data;
+document.getElementById("visitors").innerHTML = data;
