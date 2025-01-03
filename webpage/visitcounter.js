0538f07dd6b06d7f4fa83f8d35fb80b0ff110c32
LoxXo <kubawa19@gmail.com>
2025-01-03 23:14:17 +0100

    reverted to old version of visitcounter.js

--- webpage/visitcounter.js
+++ webpage/visitcounter.js
@@ -1,7 +1,11 @@
-var data
-window.onload = () => {
+function showVisitors() {
     fetch('https://http-trigger-cosmos-resume.azurewebsites.net/api/new_visitor?')
-        .then(response => console.log(response.status) || response)
-        .then(response => (data = response.text()))
-    document.getElementById("visitors").innerHTML = data;
+        .then(function (response) { return response.text(); })
+        .then(function (text) {
+            document.getElementById("visitors").innerHTML = text;
+         })
+        .catch(() => {
+            document.getElementById("visitors").innerHTML = "Error";
+        });
 }
+window.onload = showVisitors()
