ab6b234ef906cc22b09a4901e8df3ada4d7e2431
LoxXo <kubawa19@gmail.com>
2025-01-07 02:47:12 +0100

    changed name of function app in visitcounter.js

--- webpage/visitcounter.js
+++ webpage/visitcounter.js
@@ -1,5 +1,5 @@
 function showVisitors() {
-    fetch('https://http-trigger-cosmos-resume.azurewebsites.net/api/new_visitor?')
+    fetch('https://func-http-trigger-resume.azurewebsites.net/api/new_visitor?')
         .then(function (response) { return response.text(); })
         .then(function (text) {
             document.getElementById("visitors").innerHTML = text;
