5f6215637da46bd4119b885b3b32b131c767a157
LoxXo <kubawa19@gmail.com>
2025-01-16 01:20:41 +0100

    little cleanup, deleted url in cypress.config.js that prefixes any cy.visist that we insert from gha variable anyway

--- tests/cypress.config.js
+++ tests/cypress.config.js
@@ -5,6 +5,5 @@ module.exports = defineConfig({
   e2e: {
     supportFile: 'cypress/support/e2e.{js,jsx,ts,tsx}',
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
-    baseUrl: 'http://127.0.0.1:8080',
   },
 })
